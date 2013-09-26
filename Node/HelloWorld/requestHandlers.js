b0e4b2526501bd91d9bea29d9fb73ba7e58932a7
jarekb84 <jarekb@gmail.com>
2013-09-26 01:06:01 -0500

    added image uploading

--- Node/HelloWorld/requestHandlers.js
+++ Node/HelloWorld/requestHandlers.js
@@ -1,4 +1,6 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring"),
+	fs = require("fs"),
+	formidable = require("formidable");
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
@@ -8,9 +10,9 @@ function start(response) {
 		'	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />',
 		'</head>',
 		'<body>',
-		'	<form action="/upload" method="post">',
-		'		<textarea name="text" rows="20" cols="60"></textarea>',
-		'		<input type="submit" value="Submit text" />',
+		'	<form action="/upload" enctype="multipart/form-data" method="post">',
+		'		<input type="file" name="upload" />',
+		'		<input type="submit" value="Upload file" />',
 		'	</form>',
 		'</body>',
 		'</html>'
@@ -21,17 +23,40 @@ function start(response) {
 	});
 	response.write(body.join(''));
 	response.end();
-
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {
-		"Content-Type": "text/plain"
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+
+	form.parse(request, function(error, fields, files) {
+		console.log('parsing done');
+		fs.rename(files.upload.path, "./tmp/test.png", function(error) {
+			if (error) {
+				fs.unlink("/tmp/test.png");
+				fs.rename(files.upload.path, "./tmp/test.png");
+			}
+		});
+
 	});
-	response.write("You've sent: " + postData);
+
+	response.writeHead(200, {
+		"Content-Type": "text/html"
+	});
+	response.write("received  image:<br/>");
+	response.write("<img  src='/show'  />");
 	response.end();
+}
+
+function show(response) {
+	console.log("Request handler 'show' was called");
+	response.writeHead(200, {
+		"Content-Type": "image/png"
+	});
+	fs.createReadStream("./tmp/test.png").pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
